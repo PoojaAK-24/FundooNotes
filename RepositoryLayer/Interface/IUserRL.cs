400aad2fefbaac0e7a5bc4f807ebfd4c0ad15bb8
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-09-12 13:07:23 +0530

    Pooja Added:User interface

--- RepositoryLayer/Interface/IUserRL.cs
+++ RepositoryLayer/Interface/IUserRL.cs
@@ -10,5 +10,5 @@ namespace BusinessLayer.Interface
         List<UserModel> getAllUsers();
         bool RegisterUser(UserModel userModel);
         bool LoginUser(LoginModel loginModel);
-     }
+    }
 }
