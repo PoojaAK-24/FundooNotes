4177d0023bbefbeb6f2d01490b6039432d794933
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-09-19 19:08:10 +0530

    Pooja Added:Notes controller,contains ispin,archive,update,delete,add,color api's

--- FundooNotes/Controllers/NotesController.cs
+++ FundooNotes/Controllers/NotesController.cs
@@ -218,7 +218,7 @@ namespace FundooNotes.Controllers
                     message = e.Message,
                     stackTrace = e.StackTrace
                 });
-            }
+             }
 
         }
 
