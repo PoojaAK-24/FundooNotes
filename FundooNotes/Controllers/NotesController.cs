e5381e7cb282eb623cf153d4a28d624c0181c7c3
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-09-22 09:01:43 +0530

    Pooja Added:Controllers for add,delete reaminder,trash,empty trash,arcchive,unarchive

--- FundooNotes/Controllers/NotesController.cs
+++ FundooNotes/Controllers/NotesController.cs
@@ -398,6 +398,7 @@ namespace FundooNotes.Controllers
             catch (Exception e)
             {
                 return BadRequest(new { success = false, message = e.Message, stackTrace = e.StackTrace });
+            
             }
         }
     }
